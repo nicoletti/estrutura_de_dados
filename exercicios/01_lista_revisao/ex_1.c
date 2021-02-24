532402ee6d2c8df6bece47bb00363f3471f5c1d8
Vitor Nicoletti <48518763+vitornicoletti@users.noreply.github.com>
2021-02-24 18:06:01 -0300

    Update ex_1.c

--- exercicios/01_lista_revisao/ex_1.c
+++ exercicios/01_lista_revisao/ex_1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int ConverteMinutos (int t, int *h, int *m){
+    *h = t / 60;
+    *m = (t % 60);
+}
+
+int main() {
+    int totalMinutos, hr, min, *p1, *p2;
+
+    p1 = &hr;
+    p2 = &min;
+
+    printf("Digite o total de minutos:\n");
+    scanf("%d", &totalMinutos);
+
+    ConverteMinutos(totalMinutos, p1, p2);
+
+    printf("Horas/minutos: %02d:%02d", hr, min);
+    return 0;
+}
